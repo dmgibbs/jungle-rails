6a16a4db4cfe8d9d0db796ab61fc510f7947c8a2
Damion Gibbs <dmgibbs@writeme.com>
2018-09-13 20:12:32 +0000

    Added Admin-Categories feature

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,34 @@
+class Admin::CategoriesController < ApplicationController
+  
+  def index
+    @categories = Category.order(id: :desc).all
+
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to [:admin , :categories]
+    else
+      render  :new
+    end
+  end
+
+  
+
+
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :text)
+  end
+
+end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,11 @@ class ProductsController < ApplicationController
     @product = Product.find params[:id]
   end
 
+  # def soldout?
+  #   @product = Product.find params[:id]
+  #   if @product.quantity == 0
+      
+  #   end
+  # end
+
 end
